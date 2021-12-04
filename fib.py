c8f733c9fe96812bfd9d68af3ebd8cbf52686ddf
Octavio Serrano <octavioserrano@Octavio-Serranos-MacBook-Pro-10.local>
2021-12-04 15:31:08 -0800

    added memoized and fixed the one at the start of the list

--- fib.py
+++ fib.py
@@ -8,9 +8,30 @@ print([fibonacci_of(n) for n in range(15)])
 
 def fib_2(n):
     if n <= 2:
-        return 1
+        return n
     return fib_2(n-1) + fib_2(n-2)
 
 print([fib_2(n) for n in range(15)])
+
+cache = {0: 0, 1: 1}
+def fib_3(n):
+    if n in cache:    #BASE CASE
+        return cache[n]
+    # compute and cashe the fibonaccci number
+    cache[n] = fib_3(n-1) + fib_3(n-2) #recursive case
+    return cache[n]
+
+print([fib_3(n) for n in range(23)])
+
+def fib_4(n, memo={0:0,1:1}):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    memo[n] = fib_4(n-1, memo) + fib_4(n-2, memo)
+    return memo[n]
+
+print([fib_4(n) for n in range(23)])
+    
 
  
