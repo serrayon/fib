fac8a6659bb0f35cbd98f9263d9bac7bf20cb0d0
Octavio Serrano <octavioserrano@Octavio-Serranos-MacBook-Pro-10.local>
2021-12-04 17:16:46 -0800

    range vs specific position

--- fib.py
+++ fib.py
@@ -21,9 +21,9 @@ def fib_3(n):
     cache[n] = fib_3(n-1) + fib_3(n-2) #recursive case
     return cache[n]
 
-print([fib_3(n) for n in range(23)])
-
-def fib_4(n, memo={0:0,1:1}):
+print([fib_3(n) for n in range(6)])
+print([fib_3(6)])
+def fib_4(n, memo={ 0:0,1:1}):
     if n in memo:
         return memo[n]
     if n <= 2:
@@ -31,7 +31,8 @@ def fib_4(n, memo={0:0,1:1}):
     memo[n] = fib_4(n-1, memo) + fib_4(n-2, memo)
     return memo[n]
 
-print([fib_4(n) for n in range(23)])
-    
+#print([fib_4(n) for n in range(23)])
+print([fib_4(6)])
+print([fib_4(n) for n in range(7)])   
 
  
