afc09b5ecb6bc00cb47d467d307f86ed259c067c
serrayon <49975993+serrayon@users.noreply.github.com>
2023-09-21 17:25:16 -0700

    Update fib.py
    error on first two, switched n>1

--- fib.py
+++ fib.py
@@ -1,6 +1,6 @@
 def fibonacci_of(n):
    if n in {0,1}: 
-        return n
+        return 1
    return fibonacci_of(n-1) + fibonacci_of(n-2)
 
 print([fibonacci_of(n) for n in range(15)])
@@ -8,7 +8,7 @@ print([fibonacci_of(n) for n in range(15)])
 
 def fib_2(n):
     if n <= 2:
-        return n
+        return 1
     return fib_2(n-1) + fib_2(n-2)
 
 print([fib_2(n) for n in range(15)])
